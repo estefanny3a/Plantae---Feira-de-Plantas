52d1f0cd1ba1d2959ce51fe213afc4f3ef9d26bd
estefanny3a <estefannyvitalinoo@gmail.com>
2025-11-22 10:44:34 -0300

    j

--- plantae/scripts.js
+++ plantae/scripts.js
@@ -1,7 +1,27 @@
-//filtros
+//filtros catalogo
 document.addEventListener("DOMContentLoaded", () => {
   const filtros = document.querySelectorAll(".filtro");
   const cards = document.querySelectorAll(".card");
+
+  filtros.forEach(botao => {
+    botao.addEventListener("click", () => {
+      const tipo = botao.getAttribute("data-filtro");
+
+      cards.forEach(card => {
+        if (tipo === "todos" || card.getAttribute("data-tipo") === tipo) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  });
+});
+
+//filtros palestras e oficinas
+document.addEventListener("DOMContentLoaded", () => {
+  const filtros = document.querySelectorAll(".cart");
+  const cards = document.querySelectorAll(".content");
 
   filtros.forEach(botao => {
     botao.addEventListener("click", () => {
